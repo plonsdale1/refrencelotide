f9b10d817acdc49a98d8971b8378ae240c867649
plonsdale1 <petex.1995@outlook.com>
2019-07-22 23:47:46 +0000

    using emojis

---
+++ assertEqual.js
@@ -0,0 +1,18 @@
+/*const assertEqual = function(a, b) {
+return a * b
+};
+
+console.assert(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.assert(assertEqual(1, 1));*/
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("😀😀😀Assertion Passed: [actual] === [expected]");
+  } else {
+    console.log("Assertion Failed: [actual] !== [expected]");
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
