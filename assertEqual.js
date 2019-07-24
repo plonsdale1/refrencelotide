220be09fbeb431edeb3f54abd1f0faaabc51206d
plonsdale1 <petex.1995@outlook.com>
2019-07-24 22:31:41 +0000

    committed assertEqual, countOnly, tail.js

--- assertEqual.js
+++ assertEqual.js
@@ -14,5 +14,5 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(`"Lighthouse Labs" + "Bootcamp"`);
 assertEqual(1, 1);
